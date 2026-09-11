13a33a1ec1b1f4a82913b8f601e78241fdfe6644
jakobkorling <jakobkorling@gmail.com>
2026-09-11 09:41:29 +0200

    Remove old skill exercise files, keep project setup

--- skill1.ts
+++
@@ -1,9 +0,0 @@
-// Order Confirmation
-type OrderCallback = () => void;
-
-const placeOrder = (firstName: string, callback: OrderCallback) => {
-    console.log(`Order placed for ${firstName}`);
-    callback();
-}; 
-
-placeOrder("Jakob", () => {console.log("Thanks for your order!")});
